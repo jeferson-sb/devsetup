2ab8dfdb5049a8f4576597da7ee1b09d161f1e11
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2024-02-17 14:35:19 -0300

    fix: pip3 command not found

--- scripts/arch/setup-arch.sh
+++ scripts/arch/setup-arch.sh
@@ -3,17 +3,24 @@ source $PWD/scripts/util.sh
 
 function update_all() {
   next "update packages"
-  sudo pacman -Syuu
+  sudo pacman -Syuu --noconfirm
   check "Your system is up-to-date"
 }
 
 # Essentials
-sudo pacman -S --needed base-devel rust libffi libyaml openssl zlib
+sudo pacman -S --needed --noconfirm base-devel rust libffi libyaml openssl zlib
 
 # System update
 update_all
+
+# Pre-requisites (python env)
+python3 -m venv env
+source env/bin/activate
+pip3 install beaupy
 
 # Install selected packages
 python3 $PWD/scripts/install_packages.py
 
 check "Your system is successfully configured :)"
+
+deactivate
--- scripts/ubuntu/setup-ubuntu.sh
+++ scripts/ubuntu/setup-ubuntu.sh
@@ -10,10 +10,21 @@ function update_all() {
   check "Your system is up-to-date"
 }
 
+# Essentials
+sudo apt install build-essential
+sudo apt install python3-venv
+
 # Update system upfront
 update_all
+
+# Pre-requisites (python env)
+python3 -m venv env
+source env/bin/activate
+pip3 install beaupy
 
 # Install selected packages
 python3 $PWD/install_packages.py
 
 check "Your system is successfully configured :)"
+
+deactivate
